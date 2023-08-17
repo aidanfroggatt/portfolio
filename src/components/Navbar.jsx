92c2c34834c7a5800238d88db7f91ae4c1502b9c
Aidan Froggatt <aidanfr@live.ca>
2023-08-17 14:54:12 -0400

    routes for pages added and navigation added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,26 @@
 import React from "react";
 import '../styles/Navbar.css';
 import {AppInfo} from "../info/AppInfo";
+import {useNavigate} from "react-router-dom";
 
 const Navbar = () => {
+    const navigate = useNavigate()
+    const goToDestination = (destination) => {
+        navigate(destination)
+    }
 
     return (
         <div className="navbar">
             <div className="my-name-container">
-                <div className="my-name">
-                    Aidan Froggatt
+                <div className="my-name" onClick={() => goToDestination(AppInfo.landingPage.route)}>
+                    {AppInfo.name}
                 </div>
             </div>
             <div className="page-container">
                 {
                     AppInfo.pages.map((page, index) => {
                         return (
-                            <div className="page" key={index}>
+                            <div className="page" key={index} onClick={() => goToDestination(page.route)}>
                                 {page.name}
                             </div>
                         )
--- src/info/AppInfo.jsx
+++ src/info/AppInfo.jsx
@@ -2,22 +2,27 @@ import React from 'react';
 import { FaLinkedin, FaGithub, FaRegEnvelopeOpen } from 'react-icons/fa';
 
 export const AppInfo = {
+    name: "Aidan Froggatt",
     landingPage : {
         heading: "Hi. I'm Aidan.",
         description: "A passionate Software Engineering Student looking to utilize my experience to design and develop solutions...",
+        route: "/",
     },
     pages : [
         {
             name: "Experience",
             description: "My work experience",
+            route: "/experience"
         },
         {
             name: "Projects",
             description: "My projects",
+            route: "/projects"
         },
         {
             name: "Contact",
             description: "Contact me",
+            route: "/contact"
         }
     ],
     socialMedia : [

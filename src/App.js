aeb63d93e8abc6c01d8265038df2a1a103c291cf
Aidan Froggatt <aidanfr@live.ca>
2023-08-17 14:57:03 -0400

    added routes for all template pages

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import {HashRouter as Router, Route, Routes} from 'react-router-dom';
 import Home from "./pages/Home";
 import './styles/App.css';
 import Navbar from "./components/Navbar";
+import Experience from "./pages/Experience";
+import Projects from "./pages/Projects";
+import Contact from "./pages/Contact";
 
 const App = () => {
   return (
@@ -11,6 +14,9 @@ const App = () => {
               <Navbar/>
               <Routes>
                   <Route exact path='/' element={<Home/>} />
+                  <Route path='/experience' element={<Experience/>}/>
+                  <Route path='/projects' element={<Projects/>}/>
+                  <Route path='/contact' element={<Contact/>}/>
               </Routes>
           </Router>
       </>

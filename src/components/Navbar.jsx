1d0d66b35ca43075d4e47656fb2e9913820edbc4
Aidan Froggatt <aidanfr@live.ca>
2023-08-17 14:23:03 -0400

    basic landing page and navbar styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import '../styles/Navbar.css';
 
 const Navbar = () => {
 
     return (
-        <div>
+        <div className="navbar">
             <h1>Navbar</h1>
         </div>
     )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import '../styles/Home.css';
 
 const Home = () => {
 
     return (
-        <div>
+        <div className='landing-page'>
             <h1>Home</h1>
             <h2>sample body text</h2>
         </div>

ca94ee963447f34cdaf3ab4a9d20727abd63474e
Aidan Froggatt <aidanfr@live.ca>
2023-08-17 13:47:59 -0400

    fixing all routes and naming of v1 portfolio and reorganzing

--- src/archive/V1/V1App.js
+++ src/archive/V1/V1App.js
@@ -1,21 +1,21 @@
-import './archive/V1/styles/V1App.css';
+import './styles/V1App.css';
 import React from "react";
 import {HashRouter as Router, Route, Routes} from 'react-router-dom';
-import V1Home from "./archive/V1/pages/V1Home";
-import V1Contact from "./archive/V1/pages/V1Contact";
-import V1Projects from "./archive/V1/pages/V1Projects";
-import V1Resume from "./archive/V1/pages/V1Resume";
-import Home from "./pages/Home";
+import V1Home from "./pages/V1Home";
+import Contact from "./pages/V1Contact";
+import Projects from "./pages/V1Projects";
+import V1Resume from "./pages/V1Resume";
+import Home from "../../pages/Home";
 
-const App = () => {
+const V1App = () => {
   return (
       <div className="flex">
           <Router>
               <Routes>
                   <Route exact path='/' element={<Home/>} />
                   <Route path='/portfolio/' element={<Home/>} />
-                  <Route path='/v1Projects' exact element={<V1Projects/>} />
-                  <Route path='/v1Contact' exact element={<V1Contact/>} />
+                  <Route path='/v1Projects' exact element={<Projects/>} />
+                  <Route path='/v1Contact' exact element={<Contact/>} />
                   <Route path='/V1resume' exact element={<V1Resume/>} />
                   <Route path='/V1Home' exact element={<V1Home/>} />
               </Routes>
@@ -24,4 +24,4 @@ const App = () => {
   );
 }
 
-export default App;
+export default V1App;
--- src/archive/V1/components/V1Arrow.jsx
+++ src/archive/V1/components/V1Arrow.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import '../styles/arrow.css';
+import '../styles/V1Arrow.css';
 
 const V1Arrow = () => {
     return (

7d4388620b2ae81aa1e5af53790a458d3b0c7940
Aidan Froggatt <aidanfr@live.ca>
2023-08-17 20:16:25 -0400

    navbar background on scroll

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import '../styles/Navbar.css';
 import {AppInfo} from "../info/AppInfo";
 import {useLocation, useNavigate} from "react-router-dom";
@@ -6,12 +6,21 @@ import {useLocation, useNavigate} from "react-router-dom";
 const Navbar = () => {
     const location = useLocation()
     const navigate = useNavigate()
+    const [navbar, setNavbar] = useState(false)
     const goToDestination = (destination) => {
         navigate(destination)
     }
+    const changeBackground = () => {
+        if (window.scrollY >= 66) setNavbar(true)
+        else setNavbar(false)
+    }
+    useEffect(() => {
+        changeBackground()
+        window.addEventListener("scroll", changeBackground)
+    }, [])
 
     return (
-        <div className="navbar">
+        <div className={navbar ? "navbar scrolled" : "navbar"}>
             <div className="navbar-my-name-container">
                 <div className="navbar-my-name" onClick={() => goToDestination(AppInfo.pages.AboutMe.route)}>
                     {AppInfo.pages.AboutMe.name}

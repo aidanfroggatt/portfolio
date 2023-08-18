a0e385afccc0cb4bb9ea2a3f1b3b96494bd43d60
Aidan Froggatt <aidanfr@live.ca>
2023-08-17 21:16:02 -0400

    fixed some footer styling

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -16,46 +16,45 @@ const Footer = () => {
             <div className="footer">
                 <div className="footer-container">
                     <img className="wave-overlay" src={wave} alt="wave overlay image"/>
-                    <div className="footer-content">
-                        <div className="thanks">
-                            Thanks for visiting!
-                        </div>
-                        <div className="footer-column-pages">
-                            {
-                                Object.keys(AppInfo.pages).map((page, index) => {
-                                    if (page === "AboutMe") return null
-                                    console.log(page.toLowerCase(), location.pathname.replaceAll("/", ""))
-                                    return (
-                                        <div
-                                            className={location.pathname.replaceAll("/", "") === page.toLowerCase() ? "footer-page footer-current" : "footer-page"}
-                                            key={index}
-                                            onClick={() => goToDestination(AppInfo.pages[page].route)}
-                                        >
-                                            {AppInfo.pages[page].name}
-                                        </div>
-                                    )
-                                })
-                            }
-                        </div>
-                        <div className="footer-column-social-media">
-                            {
-                                Object.keys(AppInfo.socialMedia).map((socialMedia, index) => {
-                                    return (
-                                        <a className="footer-social-media-icon"
-                                           href={AppInfo.socialMedia[socialMedia].url}
-                                           target="_blank"
-                                           rel="noreferrer"
-                                           key={index}
-                                        >
-                                            {AppInfo.socialMedia[socialMedia].icon}
-                                        </a>
-                                    )
-                                })
-                            }
-                        </div>
+                </div>
+                <div className="footer-content">
+                    <div className="thanks">
+                        Thanks for visiting!
+                    </div>
+                    <div className="footer-column-pages">
+                        {
+                            Object.keys(AppInfo.pages).map((page, index) => {
+                                if (page === "AboutMe") return null
+                                console.log(page.toLowerCase(), location.pathname.replaceAll("/", ""))
+                                return (
+                                    <div
+                                        className={location.pathname.replaceAll("/", "") === page.toLowerCase() ? "footer-page footer-current" : "footer-page"}
+                                        key={index}
+                                        onClick={() => goToDestination(AppInfo.pages[page].route)}
+                                    >
+                                        {AppInfo.pages[page].name}
+                                    </div>
+                                )
+                            })
+                        }
+                    </div>
+                    <div className="footer-column-social-media">
+                        {
+                            Object.keys(AppInfo.socialMedia).map((socialMedia, index) => {
+                                return (
+                                    <a className="footer-social-media-icon"
+                                       href={AppInfo.socialMedia[socialMedia].url}
+                                       target="_blank"
+                                       rel="noreferrer"
+                                       key={index}
+                                    >
+                                        {AppInfo.socialMedia[socialMedia].icon}
+                                    </a>
+                                )
+                            })
+                        }
                     </div>
                 </div>
-
             </div>
         )
 }

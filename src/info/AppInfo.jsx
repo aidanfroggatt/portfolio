3886ed31c535dc1cd9ba3f6280df041b4592e8df
Aidan Froggatt <aidanfr@live.ca>
2023-08-17 22:08:20 -0400

    setup structure for experience and footer, updated app info

--- src/info/AppInfo.jsx
+++ src/info/AppInfo.jsx
@@ -17,7 +17,68 @@ export const AppInfo = {
         Projects: {
             name: "Projects",
             description: "Some projects I have been developing in my spare time...",
-            route: "/projects"
+            route: "/projects",
+            projects: {
+                "SnapCycle": {
+                    name: "SnapCycle",
+                    description: "A mobile application that allows users to take a picture of their waste and receive information on how to properly dispose of it.",
+                    technologies: ["React JS", "Jira", "Git", "Figma"],
+                    github: "https://github.com/snap-cycle/SnapCycle",
+                    website: "",
+                    image: "",
+                    additionalInfo: {
+                        association: "MacHacks3",
+                        devpost: "https://devpost.com/software/snapcycle-hyx3qv",
+                        awards: "Best Environmental Hack"
+                    }
+                },
+                "CodeWarriors": {
+                    name: "CodeWarriors",
+                    description: "A website that allows users to compete in coding challenges and view their progress.",
+                    technologies: ["React JS", "Tailwind CSS", "Figma"],
+                    github: "https://github.com/aidanfroggatt/CodeWarriors",
+                    website: "",
+                    image: "",
+                    additionalInfo: {
+                        devpost: "https://devpost.com/software/code-warriors",
+                        association: "DeltaHacks IX",
+                        awards: "#1 Overall Hack"
+                    }
+                },
+                "Portfolio": {
+                    name: "Portfolio",
+                    description: "A website that showcases my experience and projects.",
+                    technologies: ["React JS, Tailwind CSS", "Figma"],
+                    github: "https://github.com/aidanfroggatt/portfolio",
+                    website: "https://aidanfroggatt.github.io/portfolio/",
+                    image: "",
+                    additionalInfo: {
+
+                    }
+                },
+                "Island Generator": {
+                    name: "Island Generator",
+                    description: "A program that generates a mesh data structure and can populate the mesh with random island generation",
+                    technologies: ["Java", "Maven", "Git"],
+                    github: "https://github.com/aidanfroggatt/IslandGenerator",
+                    website: "",
+                    image: "",
+                    additionalInfo: {
+                        association: "McMaster University",
+                    }
+                },
+                "Piraten Kapern Simulator": {
+                    name: "Piraten Kapern Simulator",
+                    description: "A program that simulates the card and dice game Piraten Kapern",
+                    technologies: ["Java", "Maven", "Git"],
+                    github: "https://github.com/aidanfroggatt/PiratenKapern",
+                    website: "",
+                    image: "",
+                    additionalInfo: {
+                        association: "McMaster University",
+                    }
+                }
+            }
         },
         Contact: {
             name: "Contact",
--- src/pages/Experience.jsx
+++ src/pages/Experience.jsx
@@ -5,9 +5,17 @@ import {AppInfo} from "../info/AppInfo";
 const Experience = () => {
 
     return (
-        <div className="landing-page">
-            <h1>{AppInfo.pages.Experience.name}</h1>
-            <h2>{AppInfo.pages.Experience.description}</h2>
+        <div className="experience">
+            <div className="landing-page">
+                <h1>{AppInfo.pages.Experience.name}</h1>
+                <h2>{AppInfo.pages.Experience.description}</h2>
+            </div>
+            <div className="experience-timeline">
+
+            </div>
+            <div className="resume-container">
+
+            </div>
         </div>
     )
 }

29435af950ca228b8194eae864dd2619c937cbd7
Aidan Froggatt <aidanfr@live.ca>
2023-08-17 21:13:28 -0400

    footer finished

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -14,44 +14,48 @@ const Footer = () => {
 
         return (
             <div className="footer">
-                <div className="wave-overlay-container">
+                <div className="footer-container">
                     <img className="wave-overlay" src={wave} alt="wave overlay image"/>
-                </div>
-                <div className="footer-content">
-                    <div className="thanks">Thanks for visiting!</div>
-                    <div className="footer-column">
-                        {
-                            Object.keys(AppInfo.pages).map((page, index) => {
-                                if (page === "AboutMe") return null
-                                return (
-                                    <div
-                                        className={location.pathname.replaceAll("/", "") === page.toLowerCase() ? "footer-item footer-current" : "footer-item"}
-                                        key={index}
-                                        onClick={() => goToDestination(AppInfo.pages[page].route)}
-                                    >
-                                        {AppInfo.pages[page].name}
-                                    </div>
-                                )
-                            })
-                        }
+                    <div className="footer-content">
+                        <div className="thanks">
+                            Thanks for visiting!
+                        </div>
+                        <div className="footer-column-pages">
+                            {
+                                Object.keys(AppInfo.pages).map((page, index) => {
+                                    if (page === "AboutMe") return null
+                                    console.log(page.toLowerCase(), location.pathname.replaceAll("/", ""))
+                                    return (
+                                        <div
+                                            className={location.pathname.replaceAll("/", "") === page.toLowerCase() ? "footer-page footer-current" : "footer-page"}
+                                            key={index}
+                                            onClick={() => goToDestination(AppInfo.pages[page].route)}
+                                        >
+                                            {AppInfo.pages[page].name}
+                                        </div>
+                                    )
+                                })
+                            }
+                        </div>
+                        <div className="footer-column-social-media">
+                            {
+                                Object.keys(AppInfo.socialMedia).map((socialMedia, index) => {
+                                    return (
+                                        <a className="footer-social-media-icon"
+                                           href={AppInfo.socialMedia[socialMedia].url}
+                                           target="_blank"
+                                           rel="noreferrer"
+                                           key={index}
+                                        >
+                                            {AppInfo.socialMedia[socialMedia].icon}
+                                        </a>
+                                    )
+                                })
+                            }
+                        </div>
                     </div>
-                    <div className="footer-column">
-                        {
-                            Object.keys(AppInfo.socialMedia).map((socialMedia, index) => {
-                                return (
-                                    <a className="footer-item"
-                                       href={AppInfo.socialMedia[socialMedia].url}
-                                       target="_blank"
-                                       rel="noreferrer"
-                                       key={index}
-                                    >
-                                        {AppInfo.socialMedia[socialMedia].icon}
-                                    </a>
-                                )
-                            })
-                        }
-                    </div>
                 </div>
+
             </div>
         )
 }

955939191ad473d8c0d5fbac5eeaa4e270f2c568
Aidan Froggatt <aidanfr@live.ca>
2023-08-17 22:19:54 -0400

    added resume to experience page, updated styling

--- src/pages/Experience.jsx
+++ src/pages/Experience.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import '../styles/Experience.css';
 import {AppInfo} from "../info/AppInfo";
+import myResume from "../assets/old-resume.pdf";
 
 const Experience = () => {
 
@@ -14,7 +15,11 @@ const Experience = () => {
 
             </div>
             <div className="resume-container">
-
+                Resume.
+                <object
+                    data={myResume}
+                    className="resume">
+                </object>
             </div>
         </div>
     )

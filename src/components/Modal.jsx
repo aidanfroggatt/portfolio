408e1763c998402aef77813610f466363aea69dc
Aidan Froggatt <aidanfr@live.ca>
2023-08-18 16:06:15 -0400

    closes modal when you click outside of it

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import '../styles/components/Modal.css';
 
-const Modal = ({ isOpen, onClose, children }) => {
+const Modal = ({ isOpen, onClose, Subheading, Heading, Body }) => {
     if (!isOpen) return null;
-
+    const handleOverlayClick = (e) => {
+        if (e.target === e.currentTarget) {
+            onClose();
+        }
+    };
     return (
-        <div className="modal-overlay">
+        <div className="modal-overlay" onClick={handleOverlayClick}>
             <div className="modal">
-                {children}
+                <h2>{Subheading}</h2>
+                <h1>{Heading}</h1>
+                <p>{Body}</p>
                 <button onClick={onClose}>Close Modal</button>
             </div>
         </div>

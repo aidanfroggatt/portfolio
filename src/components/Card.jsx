d3eefe51c9a5e7c4379cbeb83fc194dd78de20a2
Aidan Froggatt <aidanfr@live.ca>
2023-08-19 17:26:02 -0400

    almost working flip cards

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -11,23 +11,11 @@ const Card = ({Heading, Subheading, Body, Image, ImageAlt, alt}) => {
         >
             <div className={alt ? "flip-card-inner flip-card-inner-alt" : "flip-card-inner"}>
                 <div className="flip-card-front">
-                    <div className="card-subheading">
-                        {Subheading}
-                    </div>
-                    <div className="card-heading">
-                        {Heading}
-                    </div>
+                    <div className="card-subheading">{Subheading}</div>
+                    <div className="card-heading">{Heading}</div>
                 </div>
                 <div className="flip-card-back">
-                    <div className="card-subheading">
-                        {Subheading}
-                    </div>
-                    <div className="card-heading">
-                        {Heading}
-                    </div>
-                    <div className="card-body">
-                        {Body}
-                    </div>
+                    <div className="card-body">{Body}</div>
                 </div>
             </div>
         </div>

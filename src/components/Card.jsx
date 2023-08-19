0386ede26d2e1438ea2c7034ca6d8b62e00f677a
Aidan Froggatt <aidanfr@live.ca>
2023-08-19 17:06:28 -0400

    basic flip card created

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,31 +1,36 @@
 import React, {useState} from "react";
 import '../styles/components/Card.css';
-import Modal from "./Modal";
 
 const Card = ({Heading, Subheading, Body, Image, ImageAlt, alt}) => {
-    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [isFlipped, setIsFlipped] = useState(false);
 
     return (
-        <div>
-            <div className={alt ? "card card-alt" : "card"} onClick={() => setIsModalOpen(true)}>
-                <div className="card-subheading">
-                    {Subheading}
+        <div
+            className={`flip-card ${isFlipped ? 'flipped' : ''}`}
+            onClick={() => setIsFlipped(!isFlipped)}
+        >
+            <div className={alt ? "flip-card-inner flip-card-inner-alt" : "flip-card-inner"}>
+                <div className="flip-card-front">
+                    <div className="card-subheading">
+                        {Subheading}
+                    </div>
+                    <div className="card-heading">
+                        {Heading}
+                    </div>
                 </div>
-                <div className="card-heading">
-                    {Heading}
+                <div className="flip-card-back">
+                    <div className="card-subheading">
+                        {Subheading}
+                    </div>
+                    <div className="card-heading">
+                        {Heading}
+                    </div>
+                    <div className="card-body">
+                        {Body}
+                    </div>
                 </div>
             </div>
-            <Modal
-                isOpen={isModalOpen}
-                onClose={() => setIsModalOpen(false)}
-                Heading={Heading}
-                Subheading={Subheading}
-                Body={Body}
-                Image={Image}
-                ImageAlt={ImageAlt}
-                alt={alt}
-            />
-            </div>
+        </div>
     )
 }
 

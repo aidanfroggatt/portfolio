880704a2b150b4106ba09d4c8aacf94ebae31337
Aidan Froggatt <aidanfr@live.ca>
2023-08-22 10:20:20 -0400

    moved navbar onto homepage

--- src/App.js
+++ src/App.js
@@ -2,14 +2,12 @@ import React from "react";
 import {HashRouter as Router, Route, Routes} from 'react-router-dom';
 import Home from "./pages/Home";
 import './styles/App.css';
-import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 
 const App = () => {
     return (
         <div className="App">
             <Router>
-                <Navbar/>
                 <Routes>
                     <Route exact path='/' element={<Home/>} />
                 </Routes>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,10 +4,12 @@ import {AppInfo} from "../info/AppInfo";
 import AnimatedPage from "../animations/AnimatedPage";
 import Experience from "../components/Experience";
 import Projects from "../components/Projects";
+import Navbar from "../components/Navbar";
 
 const Home = () => {
     return (
         <AnimatedPage>
+            <Navbar/>
             <div className='landing-page'>
                 <h1>{AppInfo.pages.AboutMe.heading}</h1>
                 <h2>{AppInfo.pages.AboutMe.description}</h2>

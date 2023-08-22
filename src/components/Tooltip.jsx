1d56ba39dd2aaf0ee7f520974ec464d977c71108
Aidan Froggatt <aidanfr@live.ca>
2023-08-22 09:54:00 -0400

    added tooltip component

---
+++ src/components/Tooltip.jsx
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+import '../components/Tooltip.css';
+
+const Tooltip = ({ children, content }) => {
+    const [show, setShow] = useState(false);
+
+    return (
+        <div
+            className="tooltip-container"
+            onMouseEnter={() => setShow(true)}
+            onMouseLeave={() => setShow(false)}
+        >
+            {children}
+            {show && <div className="tooltip-content">{content}</div>}
+        </div>
+    );
+};
+
+export default Tooltip;

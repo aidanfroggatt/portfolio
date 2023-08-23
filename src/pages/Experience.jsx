766ddaea2afcfafede270661343a729cd9738429
Aidan Froggatt <aidanfr@live.ca>
2023-08-23 16:47:17 -0400

    scroll into view animation on experience cards

--- src/pages/Experience.jsx
+++ src/pages/Experience.jsx
@@ -2,6 +2,7 @@ import React, {useEffect, useRef} from 'react';
 import '../styles/pages/Experience.css';
 import {AppInfo} from "../info/AppInfo";
 import Tooltip from "../components/Tooltip";
+import {motion} from "framer-motion";
 
 const Experience = ({setExperienceRef}) => {
     const scrollToRef = useRef(null);
@@ -17,37 +18,48 @@ const Experience = ({setExperienceRef}) => {
                     Object.keys(AppInfo.pages.Experience.timeline).map((item, index) => {
                         const alt = index % 2 === 0
                         return (
-                            <div className={alt ? "experience-card" : "experience-card experience-card-alt"}>
-                                <div className="experience-card-date-container">
-                                    <div className="experience-card-date">
-                                        {AppInfo.pages.Experience.timeline[item].date}
+                            <motion.div
+                                initial="hidden"
+                                whileInView="visible"
+                                viewport={{ once: true }}
+                                transition={{ duration: 0.3 }}
+                                variants={{
+                                    visible: { opacity: 1, scale: 1 },
+                                    hidden: { opacity: 0, scale: 0 }
+                                }}
+                            >
+                                <div className={alt ? "experience-card" : "experience-card experience-card-alt"}>
+                                    <div className="experience-card-date-container">
+                                        <div className="experience-card-date">
+                                            {AppInfo.pages.Experience.timeline[item].date}
+                                        </div>
+                                    </div>
+                                    <div className="experience-card-subheading">{AppInfo.pages.Experience.timeline[item].company}</div>
+                                    <div className="experience-card-heading">{AppInfo.pages.Experience.timeline[item].role}</div>
+                                    <div className="experience-card-bullet-container">
+                                        {
+                                            AppInfo.pages.Experience.timeline[item].bulletPoints.map((bullet, bulletKey) => {
+                                                return (
+                                                    <div key={bulletKey} className="experience-card-bullet">{bullet}</div>
+                                                )
+                                            })
+                                        }
+                                    </div>
+                                    <div className={alt ? "experience-card-skills-container" : "experience-card-skills-container experience-card-skills-container-alt"}>
+                                        {
+                                            Object.keys(AppInfo.pages.Experience.timeline[item].technologies).map((tech, techKey) => {
+                                                return (
+                                                    <Tooltip content={AppInfo.pages.Experience.timeline[item].technologies[tech].name}>
+                                                        <div key={techKey} className="experience-card-skill">
+                                                            {AppInfo.pages.Experience.timeline[item].technologies[tech].icon}
+                                                        </div>
+                                                    </Tooltip>
+                                                )
+                                            })
+                                        }
                                     </div>
                                 </div>
-                                <div className="experience-card-subheading">{AppInfo.pages.Experience.timeline[item].company}</div>
-                                <div className="experience-card-heading">{AppInfo.pages.Experience.timeline[item].role}</div>
-                                <div className="experience-card-bullet-container">
-                                    {
-                                        AppInfo.pages.Experience.timeline[item].bulletPoints.map((bullet, bulletKey) => {
-                                            return (
-                                                <div key={bulletKey} className="experience-card-bullet">{bullet}</div>
-                                            )
-                                        })
-                                    }
-                                </div>
-                                <div className={alt ? "experience-card-skills-container" : "experience-card-skills-container experience-card-skills-container-alt"}>
-                                    {
-                                        Object.keys(AppInfo.pages.Experience.timeline[item].technologies).map((tech, techKey) => {
-                                            return (
-                                                <Tooltip content={AppInfo.pages.Experience.timeline[item].technologies[tech].name}>
-                                                    <div key={techKey} className="experience-card-skill">
-                                                        {AppInfo.pages.Experience.timeline[item].technologies[tech].icon}
-                                                    </div>
-                                                </Tooltip>
-                                            )
-                                        })
-                                    }
-                                </div>
-                            </div>
+                            </motion.div>
                         )
                     })
                 }

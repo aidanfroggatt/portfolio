31037fe75a1c18234099e9775967764f6b12af2a
Aidan Froggatt <aidanfr@live.ca>
2023-08-23 15:29:32 -0400

    skill coins separated into component

--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -1,15 +1,14 @@
 import React, {useEffect, useRef} from 'react';
 import {AppInfo} from "../info/AppInfo";
 import '../styles/pages/Skills.css';
-import Tooltip from "../components/Tooltip";
+import SkillCoin from "../components/SkillCoin";
 
 const Skills = ({setSkillsRef}) => {
     const scrollToRef = useRef(null);
-
     useEffect(() => {
         setSkillsRef(scrollToRef);
     }, [setSkillsRef]);
-    //                                 {AppInfo.pages.Skills[skill].name}
+
     return (
         <div ref={scrollToRef} className="skills">
             <div className="skills-container">
@@ -17,12 +16,7 @@ const Skills = ({setSkillsRef}) => {
                     Object.keys(AppInfo.pages.Skills).map((skill, index) => {
                         const alt = index % 2 === 0
                         return (
-                                <div key={index} className={alt ? "skills-item" : "skills-item skills-item-alt"}>
-                                    <Tooltip content={AppInfo.pages.Skills[skill].name} alt={alt}>
-                                        {AppInfo.pages.Skills[skill].icon}
-                                    </Tooltip>
-                                </div>
-
+                            <SkillCoin icon={AppInfo.pages.Skills[skill].icon} skill={skill} alt={alt}/>
                         )
                     })
                 }

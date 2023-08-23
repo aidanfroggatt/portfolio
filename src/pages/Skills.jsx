2cf326666868bca0458b9bb6ad687cbfa2885b5a
Aidan Froggatt <aidanfr@live.ca>
2023-08-22 21:49:52 -0400

    added tool tip to skills coins

--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -1,6 +1,7 @@
 import React, {useEffect, useRef} from 'react';
 import {AppInfo} from "../info/AppInfo";
 import '../styles/pages/Skills.css';
+import Tooltip from "../components/Tooltip";
 
 const Skills = ({setSkillsRef}) => {
     const scrollToRef = useRef(null);
@@ -8,17 +9,20 @@ const Skills = ({setSkillsRef}) => {
     useEffect(() => {
         setSkillsRef(scrollToRef);
     }, [setSkillsRef]);
-
+    //                                 {AppInfo.pages.Skills[skill].name}
     return (
         <div ref={scrollToRef} className="skills">
             <div className="skills-container">
                 {
                     Object.keys(AppInfo.pages.Skills).map((skill, index) => {
+                        const alt = index % 2 === 0
                         return (
-                            <div key={index} className="skills-item">
-                                {AppInfo.pages.Skills[skill].icon}
-                                {AppInfo.pages.Skills[skill].name}
-                            </div>
+                                <div key={index} className={alt ? "skills-item" : "skills-item skills-item-alt"}>
+                                    <Tooltip content={AppInfo.pages.Skills[skill].name} alt={alt}>
+                                        {AppInfo.pages.Skills[skill].icon}
+                                    </Tooltip>
+                                </div>
+
                         )
                     })
                 }

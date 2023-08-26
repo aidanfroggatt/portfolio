91f8c6a54c1b694acf307acb5e03a1627c767197
Aidan Froggatt <aidanfr@live.ca>
2023-08-26 11:14:38 -0400

    hamburger menu now handled as part of navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,9 @@ import React, {useEffect, useState} from "react";
 import '../styles/components/Navbar.css';
 import {AppInfo} from "../info/AppInfo";
 import {BrowserView, MobileView} from "react-device-detect";
-import HamburgerMenu from "./HamburgerMenu";
+import HamburgerIcon from "../assets/components/HamburgerMenu.svg";
+import Logo from "../assets/logo.png";
+import WhiteX from "../assets/components/WhiteX.svg";
 
 const Navbar = ({experienceRef, projectsRef, skillsRef, handleScrollToHome}) => {
     const [navbar, setNavbar] = useState(false)
@@ -36,6 +38,26 @@ const Navbar = ({experienceRef, projectsRef, skillsRef, handleScrollToHome}) => 
                 behavior: 'smooth',
                 block: 'start',
             });
+        }
+    };
+
+    const [showHamburgerMenu, setHamburgerMenu] = useState(false);
+
+    const updateHamburgerMenu = () => {
+        setHamburgerMenu(showHamburgerMenu => !showHamburgerMenu);
+        SwitchScrollBar();
+    };
+
+    const SwitchScrollBar = () => {
+        if (showHamburgerMenu) {
+            document.body.style.overflow = 'auto';
+            document.querySelector('#HamburgerMenuActive').style.overflow = 'hidden';
+        }
+        else {
+            setTimeout(() => {
+                document.body.style.overflow = 'hidden';
+                document.querySelector('#HamburgerMenuActive').style.overflow = 'auto';
+            }, 500 );
         }
     };
 
@@ -84,7 +106,52 @@ const Navbar = ({experienceRef, projectsRef, skillsRef, handleScrollToHome}) => 
             </BrowserView>
             <MobileView>
                 <div className={"phone-navbar"}>
-                    <HamburgerMenu/>
+                    <div>
+                        <img src={HamburgerIcon} alt="Hamburger Icon" className='HamburgerIcon' onClick={() => updateHamburgerMenu()}/>
+                        <div className='HamburgerMenuContainer' id={showHamburgerMenu ? 'HamburgerMenuActive' : 'HamburgerMenuInactive'}>
+                            <div className='HamburgerMenu'>
+                                <div className='BurgerTopContainer' id={ showHamburgerMenu ? 'BurgerTopContainerActive' : 'BurgerTopContainerInactive'}>
+                                    <div className='BurgerLogoAndName'>
+                                        <img src={Logo} alt="Aidan Froggatt Logo" className="HamburgerLogo"/>
+                                        <div className='BurgerTitleContainer'>
+                                            <div className='cover' id={showHamburgerMenu && 'LogoActive'}></div>
+                                        </div>
+                                    </div>
+                                    <img src={WhiteX} alt="Close Button" className='WhiteX' onClick={() => updateHamburgerMenu()}/>
+                                </div>
+                                <div className='HamburgerPages'>
+                                    {
+                                        Object.keys(AppInfo.pages).map((key, index) => {
+                                            if (key === 'AboutMe') return;
+                                            return (
+                                                <div className='HamburgerPage' key={index}>
+                                                    <a href={AppInfo.pages[key].link} className='HamburgerPageLink' onClick={() => updateHamburgerMenu}>
+                                                        {key}
+                                                    </a>
+                                                </div>
+                                            );
+                                        })
+                                    }
+                                </div>
+                                <div className="navbar-social-media-container-hamburger">
+                                    {
+                                        Object.keys(AppInfo.socialMedia).map((socialMedia, index) => {
+                                            return (
+                                                <a className="navbar-social-media-icon-hamburger"
+                                                   href={AppInfo.socialMedia[socialMedia].url}
+                                                   target="_blank"
+                                                   rel="noreferrer"
+                                                   key={index}
+                                                >
+                                                    {AppInfo.socialMedia[socialMedia].icon}
+                                                </a>
+                                            )
+                                        })
+                                    }
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </MobileView>
         </>

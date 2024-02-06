ea622f87959ba5afa25e79e41b41dbb9e4f8efe6
Aidan Froggatt <aidanfr@live.ca>
2024-02-06 17:09:52 -0500

    added footprint preloader

--- src/App.js
+++ src/App.js
@@ -1,19 +1,34 @@
-import React from "react";
-import {HashRouter as Router, Route, Routes} from 'react-router-dom';
+import {HashRouter as Router, Route, Routes} from 'react-router-dom'
+import React, { useState, useEffect } from 'react';
 import Home from "./pages/Home";
 import './styles/App.css';
 import Preloader from "./components/Preloader";
 
 const App = () => {
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        // Simulate loading process
+        setTimeout(() => {
+            setLoading(false);
+        }, 2000); // Adjust the time as per your need
+    }, []);
+
     return (
         <>
-            <div className="App">
-                <Router>
-                    <Routes>
-                        <Route exact path='/' element={<Home/>} />
-                    </Routes>
-                </Router>
-            </div>
+            {
+                loading ?
+                <Preloader/>
+                    :
+                <div className="App">
+                    <Router>
+                        <Routes>
+                            <Route exact path='/' element={<Home/>} />
+                        </Routes>
+                    </Router>
+                </div>
+            }
+
         </>
     );
 }

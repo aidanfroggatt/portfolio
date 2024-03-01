dbaaa4cc06fae3e679cdc898884c038e915ed6f7
Aidan Froggatt <aidanfr@live.ca>
2024-03-01 15:26:15 -0500

    removed preloader to streamline experience

--- src/App.js
+++ src/App.js
@@ -1,35 +1,17 @@
 import {HashRouter as Router, Route, Routes} from 'react-router-dom'
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import Home from "./pages/Home";
 import './styles/App.css';
-import Preloader from "./components/Preloader";
 
 const App = () => {
-    const [loading, setLoading] = useState(true);
-
-    useEffect(() => {
-        // Simulate loading process
-        setTimeout(() => {
-            setLoading(false);
-        }, 2000); // Adjust the time as per your need
-    }, []);
-
     return (
-        <>
-            {
-                loading ?
-                <Preloader/>
-                    :
-                <div className="App">
-                    <Router>
-                        <Routes>
-                            <Route exact path='/' element={<Home/>} />
-                        </Routes>
-                    </Router>
-                </div>
-            }
-
-        </>
+        <div className="App">
+            <Router>
+                <Routes>
+                    <Route exact path='/' element={<Home/>} />
+                </Routes>
+            </Router>
+        </div>
     );
 }
 

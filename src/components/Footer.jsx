755775026e2f3fc4baf3906b0ee6598ac7803ab3
Aidan Froggatt <aidanfr@live.ca>
2024-04-19 00:02:02 -0400

    footer functionality

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,13 @@
 import LilypadIcon from "../assets/LilypadIcon.jsx";
+import {useNavigate} from "react-router-dom";
 
 const Footer = () => {
-
+    const navigate = useNavigate();
+    const handleNavigate = (destination) => {
+        navigate(destination);
+    }
     return (
-        <div className="relative bottom-0 bg-custom-dark text-custom-light border-t border-gray-500 h-80 justify-evenly items-center flex flex-row py-20 px-40 gap-x-32">
+        <div className="relative bottom-0 bg-custom-dark text-custom-light border-t border-gray-500 h-80 justify-evenly items-center flex flex-row py-20 px-40 gap-x-32 select-none">
             <div className="h-full w-full flex flex-col justify-between">
                 <div className="flex h-full">
                     <LilypadIcon className={"w-20 h-20"}/>
@@ -15,15 +19,15 @@ const Footer = () => {
             </div>
             <div className="h-full w-full flex flex-col justify-between items-end">
                 <div className="flex flex-row justify-end items-start gap-x-16 w-full h-full">
-                    <div className="flex flex-col justify-center items-center">
+                    <div className="flex flex-col justify-center items-center gap-y-4">
                         <text className="text-xs text-gray-500">MAIN</text>
-                        <text className="text-sm">Work</text>
-                        <text className="text-sm">Info</text>
+                        <text className="text-sm hover:cursor-pointer" onClick={() => handleNavigate("/")}>Work</text>
+                        <text className="text-sm hover:cursor-pointer" onClick={() => handleNavigate("/info")}>Info</text>
                     </div>
-                    <div className="flex flex-col justify-center items-center">
+                    <div className="flex flex-col justify-center items-center gap-y-4">
                         <text className="text-xs text-gray-500">CONTACT</text>
-                        <text className="text-sm">LinkedIn</text>
-                        <text className="text-sm">Resume</text>
+                        <a className="text-sm" href="https://www.linkedin.com/in/aidanfroggatt/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+                        <a className="text-sm" href="" target="_blank" rel="noopener noreferrer">Resume</a>
                     </div>
                 </div>
                 <div className="flex flex-col h-full w-full items-end justify-end">

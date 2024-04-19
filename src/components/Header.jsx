6d378a406f600f89384fcd9c129ab776b883c9f6
Aidan Froggatt <aidanfr@live.ca>
2024-04-19 00:15:07 -0400

    added tabs to navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -20,18 +20,20 @@ const Header = () => {
                     <text className="text-xs text-gray-500">Frontend Developer</text>
                 </div>
             </div>
-            <div className="flex justify-center items-center font-medium bg-white p-1.5 rounded-full bg-opacity-10 text-sm border border-opacity-10 border-white backdrop-blur hover:border-opacity-20">
+            <div className="flex relative justify-center items-center font-medium bg-white p-1.5 rounded-full bg-opacity-10 text-sm border border-opacity-10 border-white backdrop-blur hover:border-opacity-20">
                 <div
                     className={`${'/' === location.pathname && 'bg-white bg-opacity-10'} flex justify-center items-center px-6 py-2 rounded-full hover:cursor-pointer`}
                     onClick={() => handleNavigate('/')}
                 >
                     <text>Work</text>
+                    {'/' === location.pathname && <div className="absolute top-0 transform -mt-0.5 w-6 h-0.5 rounded-t-full bg-custom-light"></div>}
                 </div>
                 <div
                     className={`${'/info' === location.pathname && 'bg-white bg-opacity-10'} flex justify-center items-center px-6 py-2 rounded-full hover:cursor-pointer`}
                     onClick={() => handleNavigate('/info')}
                 >
                     <text>Info</text>
+                    {'/info' === location.pathname && <div className="absolute top-0 transform -mt-0.5 w-6 h-0.5 rounded-t-full bg-custom-light"></div>}
                 </div>
             </div>
             <div className="flex font-medium">

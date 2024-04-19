d768d5da9192bbfc977946fdf6ea30ca0de5c89f
Aidan Froggatt <aidanfr@live.ca>
2024-04-18 22:38:46 -0400

    added links, name, icon to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,24 @@
+import LilypadIcon from "../assets/LilypadIcon.jsx";
+import ArrowLink from "./ArrowLink.jsx";
 
 
 const Header = () => {
 
     return (
-        <div className="flex flex-row justify-between">
-            <div>
-                name n stuff
+        <div className="flex flex-row fixed justify-between items-center h-20 w-full bg-red-200 py-8 px-12">
+            <div className="flex flex-row bg-blue-200 gap-x-2">
+                <LilypadIcon className={"w-10 h-10"}/>
+                <div className="flex flex-col">
+                    <text className="font-semibold text-md">Aidan Froggatt</text>
+                    <text className="text-xs">Frontend Developer</text>
+                </div>
             </div>
-            <div>
-                nav n stuff
+            <div className="flex justify-center items-center bg-green-200">
+                <text>nav n stuff</text>
             </div>
-            <div>
-                contact n stuff
+            <div className="flex bg-yellow-200">
+                <ArrowLink destination="https://www.linkedin.com/in/aidanfroggatt/" title="LinkedIn"/>
+                <ArrowLink destination="" title="Resume"/>
             </div>
         </div>
     )

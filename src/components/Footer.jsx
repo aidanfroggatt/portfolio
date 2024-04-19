715f9542bb399a7aeee0743cd9f2c3499c3ef5a6
Aidan Froggatt <aidanfr@live.ca>
2024-04-19 00:55:57 -0400

    updated colors

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -21,31 +21,31 @@ const Footer = () => {
 
 
     return (
-        <div className="relative bottom-0 bg-custom-dark text-custom-light border-t border-gray-500 h-80 justify-evenly items-center flex flex-row py-20 px-40 gap-x-32">
+        <div className="relative bottom-0 bg-custom-dark text-custom-light border-t border-custom-light border-opacity-50 h-80 justify-evenly items-center flex flex-row py-20 px-40 gap-x-32">
             <div className="h-full w-full flex flex-col justify-between">
                 <div className="flex h-full">
                     <LilypadIcon className={"w-20 h-20"}/>
                 </div>
                 <div className="flex flex-col h-full w-full justify-end items-start">
                     <text className="font-semibold text-md">Aidan Froggatt</text>
-                    <text className="text-sm text-gray-500">Thanks for visiting</text>
+                    <text className="text-sm text-custom-light text-opacity-50">Thanks for visiting</text>
                 </div>
             </div>
             <div className="h-full w-full flex flex-col justify-between items-end">
                 <div className="flex flex-row justify-end items-start gap-x-16 w-full h-full">
                     <div className="flex flex-col justify-center items-center gap-y-4">
-                        <text className="text-xs text-gray-500">MAIN</text>
+                        <text className="text-xs text-custom-light text-opacity-50">MAIN</text>
                         <text className="text-sm hover:cursor-pointer" onClick={() => handleNavigate("/")}>Work</text>
                         <text className="text-sm hover:cursor-pointer" onClick={() => handleNavigate("/info")}>Info</text>
                     </div>
                     <div className="flex flex-col justify-center items-center gap-y-4">
-                        <text className="text-xs text-gray-500">CONTACT</text>
+                        <text className="text-xs text-custom-light text-opacity-50">CONTACT</text>
                         <a className="text-sm" href="https://www.linkedin.com/in/aidanfroggatt/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
                         <a className="text-sm" href="" target="_blank" rel="noopener noreferrer">Resume</a>
                     </div>
                 </div>
                 <div className="flex flex-col h-full w-full items-end justify-end">
-                    <text className="text-sm text-gray-500">Last updated by Aidan, {lastUpdate}</text>
+                    <text className="text-sm text-custom-light text-opacity-50">Last updated by Aidan, {lastUpdate}</text>
                 </div>
             </div>
         </div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,4 @@
 import LilypadIcon from "../assets/LilypadIcon.jsx";
-import ArrowLink from "./ArrowLink.jsx";
 import {useLocation, useNavigate} from "react-router-dom";
 
 
@@ -17,19 +16,19 @@ const Header = () => {
                 <LilypadIcon className={"w-10 h-10"}/>
                 <div className="flex flex-col">
                     <text className="font-semibold text-md">Aidan Froggatt</text>
-                    <text className="text-xs text-gray-500">Frontend Developer</text>
+                    <text className="text-xs text-custom-light text-opacity-50">Frontend Developer</text>
                 </div>
             </div>
-            <div className="flex justify-center items-center font-medium bg-white p-1.5 rounded-full bg-opacity-10 text-sm border border-opacity-10 border-white backdrop-blur hover:border-opacity-20">
+            <div className="flex justify-center items-center font-medium bg-custom-light p-1.5 rounded-full bg-opacity-10 text-sm border border-opacity-10 border-custom-light backdrop-blur hover:border-opacity-20">
                 <div
-                    className={`${'/' === location.pathname && 'bg-white bg-opacity-10'} flex justify-center items-center px-6 py-2 rounded-full hover:cursor-pointer`}
+                    className={`${'/' === location.pathname && 'bg-custom-light bg-opacity-10'} flex justify-center items-center px-6 py-2 rounded-full hover:cursor-pointer`}
                     onClick={() => handleNavigate('/')}
                 >
                     <text>Work</text>
                     {'/' === location.pathname && <div className="absolute top-0 transform -mt-0.5 w-6 h-0.5 rounded-t-full bg-custom-light"></div>}
                 </div>
                 <div
-                    className={`${'/info' === location.pathname && 'bg-white bg-opacity-10'} flex justify-center items-center px-6 py-2 rounded-full hover:cursor-pointer`}
+                    className={`${'/info' === location.pathname && 'bg-custom-light bg-opacity-10'} flex justify-center items-center px-6 py-2 rounded-full hover:cursor-pointer`}
                     onClick={() => handleNavigate('/info')}
                 >
                     <text>Info</text>
@@ -37,8 +36,9 @@ const Header = () => {
                 </div>
             </div>
             <div className="flex font-medium">
-                <ArrowLink destination="https://www.linkedin.com/in/aidanfroggatt/" title="LinkedIn"/>
-                <ArrowLink destination="" title="Resume"/>
+                <a className="text-sm px-3 py-2 hover:bg-custom-light hover:bg-opacity-10 rounded-full" href="https://www.linkedin.com/in/aidanfroggatt/" target="_blank"
+                   rel="noopener noreferrer">LinkedIn</a>
+                <a className="text-sm px-3 py-2 hover:bg-custom-light hover:bg-opacity-10 rounded-full" href="" target="_blank" rel="noopener noreferrer">Resume</a>
             </div>
         </div>
     )

eee3903e067e6ae86396f8ca396e1c5146ca4084
Aidan Froggatt <aidanfr@live.ca>
2024-04-20 19:08:38 -0400

    all routing scrolls to top of page

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import {BrowserRouter as Router, Route, Routes, useLocation} from 'react-router-dom';
 import { GeneralInfoProvider } from './contexts/GeneralInfoContext.jsx';
 import InfoPage from './pages/InfoPage.jsx';
 import WorkPage from "./pages/WorkPage.jsx";
@@ -6,16 +6,28 @@ import ProjectPage from './pages/ProjectPage.jsx';
 import Header from './components/Header.jsx';
 import Footer from './components/Footer.jsx';
 import './App.css';
+import {useEffect} from "react";
+
+function ScrollToTop() {
+    const { pathname } = useLocation();
+
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, [pathname]);
+
+    return null;
+}
 
 function App() {
     return (
         <GeneralInfoProvider>
             <Router>
                 <Header/>
+                <ScrollToTop/>
                 <Routes>
                     <Route path="/" element={<WorkPage/>} />
                     <Route path="/info" element={<InfoPage/>} />
-                    <Route path="/project/:projectID" element={<ProjectPage/>}/>
+                    <Route path="/project/:projectId" element={<ProjectPage/>}/>
                 </Routes>
                 <Footer/>
             </Router>
--- src/pages/ProjectPage.jsx
+++ src/pages/ProjectPage.jsx
@@ -1,12 +1,21 @@
 import '../styles/pages/ProjectPage.css';
 import {useParams} from "react-router-dom";
+import {useEffect, useState} from "react";
+import {getDataFromFirestore} from "../utils/firestoreUtils.js";
 
 const ProjectPage = () => {
-    const { id } = useParams();
+    const { projectId } = useParams();
+    const [projectInfo, setProjectInfo] = useState();
+
+    useEffect(() => {
+        getDataFromFirestore({collectionName: 'projects', documentId: projectId}).then(data => setProjectInfo(data));
+    }, []);
+
+    console.log(projectInfo);
 
     return (
         <div className="project-page min-h-screen bg-custom-dark flex justify-center items-center text-custom-light">
-            test: {id}
+            test: {projectId}
         </div>
     )
 }

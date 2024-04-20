45e2772ac2d09731cf09ce6ccd38eda2e5a88d77
Aidan Froggatt <aidanfr@live.ca>
2024-04-20 13:45:15 -0400

    project data queried from firebase

--- src/pages/WorkPage.jsx
+++ src/pages/WorkPage.jsx
@@ -1,21 +1,44 @@
 import MacWindowCard from "../components/MacWindowCard.jsx";
 import ProjectCard from "../components/ProjectCard.jsx";
 import {useNavigate} from "react-router-dom";
+import {useEffect, useState} from "react";
+import {getDocs, collection} from "firebase/firestore";
+import {firestore} from ".././config/firebase.config.js";
 
 const WorkPage = () => {
     const navigate = useNavigate()
     const handleProjectClick = ({projectID}) => {
         navigate(`/project/${projectID}`);
     }
+
+    const [projects, setProjects] = useState([]);
+
+    useEffect(() => {
+        const getProjects = async () => {
+            const querySnapshot = await getDocs(collection(firestore, 'projects'));
+            const projectsData = [];
+            querySnapshot.forEach((doc) => {
+                projectsData.push({ id: doc.id, ...doc.data() });
+            });
+            setProjects(projectsData);
+        }
+        getProjects().then(r => console.log('projects fetched')).catch(e => console.error(e));
+    }, []);
+
     return (
         <div className="min-h-screen flex flex-col justify-evenly items-center bg-custom-dark text-custom-light" style={{paddingTop: '25vh', paddingBottom: '25vh', gap: '7.5vh'}}>
             <MacWindowCard/>
-            <ProjectCard
-                title="SnapCycle"
-                association="MacHacks3"
-                description="AI recylability detection"
-                handleClick={() => handleProjectClick({projectID: 'snapcycle'})}
-            />
+            {
+                projects.map((project) => (
+                    <ProjectCard
+                        key={project.id}
+                        title={project.title}
+                        association={project.association}
+                        description={project.description}
+                        handleClick={() => handleProjectClick({projectID: project.id})}
+                    />
+                ))
+            }
         </div>
     )
 }

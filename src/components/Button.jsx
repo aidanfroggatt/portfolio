698d2c4d1ff60a48fa13777a8e4483f571e5df44
Aidan Froggatt <aidanfr@live.ca>
2024-04-20 23:02:06 -0400

    updated back button styling

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@ import '../styles/components/Button.css';
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faArrowRightLong, faArrowLeftLong} from "@fortawesome/free-solid-svg-icons";
 
-const Button = ({leftArrow, rightArrow, title, handleClick, className}) => {
+const Button = ({leftArrow, rightArrow, title, handleClick}) => {
 
     return (
         <div

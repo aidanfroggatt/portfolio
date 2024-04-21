81e08c0823667f7a8dd6d0c8540d1e222ed3df68
Aidan Froggatt <aidanfr@live.ca>
2024-04-20 23:05:19 -0400

    updated back button styling

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -6,7 +6,7 @@ const Button = ({leftArrow, rightArrow, title, handleClick}) => {
 
     return (
         <div
-            className="button cursor-pointer min-w-20 min-h-8 flex flex-row gap-x-2 items-center fixed left-12 px-3 py-2 bg-custom-light bg-opacity-10 border-custom-light border border-opacity-20 rounded-full hover:bg-opacity-30 transition-all duration-300 ease-in-out z-50"
+            className="button cursor-pointer min-w-20 min-h-8 flex flex-row gap-x-2 items-center fixed left-12 px-4 py-2 bg-custom-light bg-opacity-10 border-custom-light border border-opacity-20 rounded-full hover:bg-opacity-30 transition-all duration-300 ease-in-out z-50"
             onClick={handleClick}
         >
             {leftArrow && <FontAwesomeIcon className="button-arrow left" icon={faArrowLeftLong}/>}

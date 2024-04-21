02a30e0b3d0ca889f87c646545d96dcab1ab8708
Aidan Froggatt <aidanfr@live.ca>
2024-04-21 13:56:38 -0400

    header persists [age content loading for work and info

--- src/pages/InfoPage.jsx
+++ src/pages/InfoPage.jsx
@@ -1,14 +1,18 @@
 import '../styles/pages/InfoPage.css';
 import Header from "../components/Header.jsx";
+import Loading from "../components/Loading.jsx";
 
 const InfoPage = () => {
+    const isLoading = false;
 
     return (
         <>
             <Header/>
-            <div className="info-page h-screen flex justify-center items-center bg-custom-dark text-custom-light">
-                <h1>Info Page</h1>
-            </div>
+            {isLoading ? <Loading/> :
+                <div className="info-page h-screen flex justify-center items-center bg-custom-dark text-custom-light">
+                    <h1>Info Page</h1>
+                </div>
+            }
         </>
     )
 }

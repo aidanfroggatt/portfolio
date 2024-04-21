5864214631bc36663189cd125378df3ee72509a9
Aidan Froggatt <aidanfr@live.ca>
2024-04-21 00:14:43 -0400

    added loading spinner

---
+++ src/components/Loading.jsx
@@ -0,0 +1,12 @@
+import '../styles/components/Loading.css';
+
+const Loading = () => {
+
+    return (
+        <div className="min-h-screen bg-custom-dark flex justify-center items-center">
+            <div className="loader"></div>
+        </div>
+    )
+}
+
+export default Loading;
--- src/pages/ProjectPage.jsx
+++ src/pages/ProjectPage.jsx
@@ -4,9 +4,10 @@ import { useEffect, useState } from "react";
 import { getDataFromFirestore } from "../utils/firestoreUtils.js";
 import { hexToRGBA } from "../utils/colorUtils.js";
 import Button from "../components/Button.jsx";
+import Loading from "../components/Loading.jsx";
 
 const ProjectPage = () => {
-    const { projectId } = useParams();
+    const {projectId} = useParams();
     const [projectInfo, setProjectInfo] = useState();
     const [isBackClicked, setIsBackClicked] = useState(false);
     const [isLoading, setIsLoading] = useState(true);
@@ -27,21 +28,25 @@ const ProjectPage = () => {
     };
 
     useEffect(() => {
-        getDataFromFirestore({ collectionName: 'projects', documentId: projectId }).then(data => {
+        getDataFromFirestore({collectionName: 'projects', documentId: projectId}).then(data => {
             setProjectInfo(transformData(data));
-            setIsLoading(false);
+            setTimeout(() => {
+                setIsLoading(false);
+            }, 500); // Adjust timing to match transition duration
         });
     }, []);
 
-    if (isLoading) return null;
-
     return (
-        <div className='project-page min-h-screen bg-custom-dark flex justify-center items-center text-custom-light' style={{ '--project-color': hexToRGBA(projectInfo.color, 0.5) }}>
-            <div className={`fixed flex flex-row justify-center items-center top-0 left-0 h-20 p-12 ${isBackClicked && 'slide-off'}`}>
-                <Button title="Back" leftArrow={true} handleClick={handleBack} />
+        isLoading ? <Loading/> :
+            <div className='project-page min-h-screen bg-custom-dark flex justify-center items-center text-custom-light'
+                 style={{'--project-color': hexToRGBA(projectInfo.color, 0.5)}}>
+                <div
+                    className={`fixed flex flex-row justify-center items-center top-0 left-0 h-20 p-12 ${isBackClicked && 'slide-off'}`}>
+                    <Button title="Back" leftArrow={true} handleClick={handleBack}/>
+                </div>
+                <div>{projectInfo.title}</div>
             </div>
-            <div>{projectInfo.title}</div>
-        </div>
+
     )
 }
 

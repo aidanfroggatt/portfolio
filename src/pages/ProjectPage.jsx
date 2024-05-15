05da058202b6bf5b6752f6d72f1c44ac191b83d4
Aidan Froggatt <aidanfr@live.ca>
2024-05-14 22:29:54 -0400

    changed highlights card icon

--- src/pages/ProjectPage.jsx
+++ src/pages/ProjectPage.jsx
@@ -10,8 +10,9 @@ import Loading from "../components/Loading.jsx";
 import Tooltip from "../components/Tooltip.jsx";
 import {calculateTimeElapsed, convertFirestoreTimestampToJSDate, formatMonthYear} from "../utils/dateTimeUtils.js";
 import HighlightCard from "../components/cards/HighlightCard.jsx";
-import {FaSeedling} from "react-icons/fa";
+import {FaFire, FaMountain, FaSeedling} from "react-icons/fa";
 import {getIconByName} from "../utils/iconUtils.jsx";
+import {FaPersonHiking} from "react-icons/fa6";
 
 const ProjectPage = () => {
     const {projectId} = useParams();
@@ -148,7 +149,9 @@ const ProjectPageHighlights = ({projectInfo}) => {
     return (
         <HighlightCard accentColor={projectInfo.color}>
             <div className="flex flex-col justify-between items-center gap-y-4">
-                <FaSeedling color={projectInfo.color} style={{width: '3vmax', height: '3vmax'}}/>
+                <div className="rounded-full bg-custom-dark bg-opacity-50 p-1.5 shadow-md shadow-custom-dark">
+                    <FaMountain color={projectInfo.color} style={{width: '3vmax', height: '3vmax'}}/>
+                </div>
                 <p className="project-page-highlights-title">
                     HIGHLIGHTS
                 </p>

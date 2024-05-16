469e71e3dc19de3d489e831e17bc6776bd1357cc
Aidan Froggatt <aidanfr@live.ca>
2024-05-16 16:23:09 -0400

    added shine animation to mac window card

--- src/components/cards/MacWindowCard.jsx
+++ src/components/cards/MacWindowCard.jsx
@@ -4,6 +4,10 @@ const MacWindowCard = ({children}) => {
     return (
         <div className="window-container">
             <div className="window-overlay"></div>
+            <div className="shine-wrapper">
+                <div className="shine-small"></div>
+                <div className="shine-big"></div>
+            </div>
             <div className="window">
                 <div className="window-header">
                     <div className="dot red"></div>

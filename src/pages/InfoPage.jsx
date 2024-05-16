a5f7aca8939ab58819d15ce50d8c09bd86375a9c
Aidan Froggatt <aidanfr@live.ca>
2024-05-16 19:22:35 -0400

    hard coded about me condtent

--- src/pages/InfoPage.jsx
+++ src/pages/InfoPage.jsx
@@ -2,10 +2,8 @@ import '../styles/pages/InfoPage.css';
 import Header from "../components/Header.jsx";
 import Loading from "../components/Loading.jsx";
 import Card from "../components/cards/Card.jsx";
-import {useEffect, useState} from "react";
-import {getDataFromFirestore} from "../utils/firestoreUtils.js";
-import {getDownloadURL, ref} from "firebase/storage";
-import {storage} from "../config/firebase.config.js";
+import { useEffect, useState } from "react";
+import { getDataFromFirestore } from "../utils/firestoreUtils.js";
 
 const InfoPage = () => {
     const [isLoading, setIsLoading] = useState(true);
@@ -21,7 +19,7 @@ const InfoPage = () => {
     };
 
     useEffect(() => {
-        getDataFromFirestore({collectionName: 'general-info', documentId: documentId}).then(data => {
+        getDataFromFirestore({ collectionName: 'general-info', documentId: documentId }).then(data => {
             setInfo(transformData(data));
             setIsLoading(false);
         });
@@ -29,35 +27,27 @@ const InfoPage = () => {
 
     return (
         <>
-            <Header/>
-            {isLoading ? <Loading/> :
+            <Header />
+            {isLoading ? <Loading /> :
                 <div className="info-page">
                     <div className="flex flex-col gap-y-8">
                         <div className="flex flex-row justify-start items-center gap-x-2">
                             <div className="dot"></div>
                             <h2 className="info-page-subtitle">ABOUT ME</h2>
                         </div>
-                        <h1 className="info-page-title">Here's some more info&nbsp;<span
-                            className="info-page-title-alt">about me.</span></h1>
+                        <h1 className="info-page-title">Here's some more info&nbsp;<span className="info-page-title-alt">about me.</span></h1>
                     </div>
-                    <div className="grid grid-cols-2 gap-20 justify-between w-full">
-                        {info.aboutMe && Object.keys(info.aboutMe).map(key => {
-                            if (info.aboutMe[key].type === 'text') {
-                                return (
-                                    <div className="info-page-text">{info.aboutMe[key].text}</div>
-                                )
-                            }
-                            else if (info.aboutMe[key].type === 'image') {
-                                    return (
-                                        <Card className="w-full h-auto" key={key}>
-                                            <img className="info-page-profile-picture" src={info.aboutMe[key].src} alt={info.aboutMe[key].alt} />
-                                        </Card>
-                                    );
-                            }
-                            else {
-                                return null;
-                            }
-                        })}
+                    <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 justify-between w-full">
+                        <div className="">
+                            <img src={info.aboutMe[0].src} alt={info.aboutMe[0].alt}/>
+                            <div className="info-page-text">{info.aboutMe[2].text}</div>
+                            <img src={info.aboutMe[4].src} alt={info.aboutMe[4].alt}/>
+                        </div>
+                        <div className="">
+                            <div className="info-page-text">{info.aboutMe[1].text}</div>
+                            <img src={info.aboutMe[3].src} alt={info.aboutMe[3].alt}/>
+                            <div className="info-page-text">{info.aboutMe[5].text}</div>
+                        </div>
                     </div>
                 </div>
             }

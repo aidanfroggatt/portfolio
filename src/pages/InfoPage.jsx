08c9bf911c4a26dbde7289698597d42cb1a0d1a5
Aidan Froggatt <aidanfr@live.ca>
2024-05-16 17:05:49 -0400

    image on info page now

--- src/pages/InfoPage.jsx
+++ src/pages/InfoPage.jsx
@@ -2,9 +2,12 @@ import '../styles/pages/InfoPage.css';
 import Header from "../components/Header.jsx";
 import Loading from "../components/Loading.jsx";
 import Card from "../components/cards/Card.jsx";
+import {useContext} from "react";
+import {GeneralInfoContext} from "../contexts/GeneralInfoContext.jsx";
 
 const InfoPage = () => {
     const isLoading = false;
+    const generalInfo = useContext(GeneralInfoContext);
 
     return (
         <>
@@ -16,12 +19,12 @@ const InfoPage = () => {
                             <div className="dot"></div>
                             <h2 className="info-page-subtitle">ABOUT ME</h2>
                         </div>
-                        <h1 className="info-page-title">This is some sort of tagline&nbsp;<span
+                        <h1 className="info-page-title">Here's a little more info&nbsp;<span
                             className="info-page-title-alt">about me.</span></h1>
                     </div>
                     <div className="flex flex-row justify-between items-stretch w-full">
                         <Card className="w-96 h-96">
-                            <h2>Picture of me</h2>
+                            {generalInfo.profilePicture && <img src={generalInfo.profilePicture.src} alt={generalInfo.profilePicture.alt}/>}
                         </Card>
                         <h2>Description of me</h2>
                     </div>

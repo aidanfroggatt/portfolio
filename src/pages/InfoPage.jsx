bb5cad8b4a07a4d175b2f8fbd3ca146814335adb
Aidan Froggatt <aidanfr@live.ca>
2024-05-16 21:07:30 -0400

    basic experience section on info page

--- src/pages/InfoPage.jsx
+++ src/pages/InfoPage.jsx
@@ -30,29 +30,64 @@ const InfoPage = () => {
             <Header />
             {isLoading ? <Loading /> :
                 <div className="info-page">
-                    <div className="flex flex-col gap-y-8">
-                        <div className="flex flex-row justify-start items-center gap-x-2">
-                            <div className="dot"></div>
-                            <h2 className="info-page-subtitle">ABOUT ME</h2>
-                        </div>
-                        <h1 className="info-page-title">Here's some more info&nbsp;<span className="info-page-title-alt">about me.</span></h1>
-                    </div>
-                    <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 justify-between w-full">
-                        <div className="">
-                            <img src={info.aboutMe[0].src} alt={info.aboutMe[0].alt}/>
-                            <div className="info-page-text">{info.aboutMe[2].text}</div>
-                            <img src={info.aboutMe[4].src} alt={info.aboutMe[4].alt}/>
-                        </div>
-                        <div className="">
-                            <div className="info-page-text">{info.aboutMe[1].text}</div>
-                            <img src={info.aboutMe[3].src} alt={info.aboutMe[3].alt}/>
-                            <div className="info-page-text">{info.aboutMe[5].text}</div>
-                        </div>
-                    </div>
+                    <AboutMe info={info}/>
+                    <Experience info={info}/>
                 </div>
             }
         </>
     )
+}
+
+const AboutMe = ({ info }) => {
+    return (
+        <>
+            <div className="flex flex-col gap-y-8">
+                <div className="flex flex-row justify-start items-center gap-x-4">
+                    <div className="dot"></div>
+                    <h2 className="info-page-subtitle">ABOUT ME</h2>
+                </div>
+                <h1 className="info-page-title">Here's some more info&nbsp;<span className="info-page-title-alt">about me.</span>
+                </h1>
+            </div>
+            <div className="grid grid-cols-1 lg:grid-cols-2 gap-x-20 justify-between w-full">
+                <div className="flex flex-col gap-y-20">
+                    <Card><img src={info.aboutMe[0].src} alt={info.aboutMe[0].alt}/></Card>
+                    <div className="info-page-text">{info.aboutMe[2].text}</div>
+                    <Card><img src={info.aboutMe[4].src} alt={info.aboutMe[4].alt}/></Card>
+                </div>
+                <div className="flex flex-col gap-y-20">
+                    <div className="info-page-text">{info.aboutMe[1].text}</div>
+                    <Card><img src={info.aboutMe[3].src} alt={info.aboutMe[3].alt}/></Card>
+                    <div className="info-page-text">{info.aboutMe[5].text}</div>
+                </div>
+            </div>
+        </>
+    );
+}
+
+const Experience = ({ info }) => {
+    return (
+        <>
+            <div className="flex flex-row justify-start items-center gap-x-4">
+                <div className="dot"></div>
+                <h2 className="info-page-subtitle">EXPERIENCE</h2>
+            </div>
+            <div className="flex flex-col w-full">
+                { info.experience && info.experience.map((exp, index) => {
+                    return (
+                        <div key={index} className="grid grid-cols-2 gap-x-20 justify-start items-start">
+                            <div className="flex-grow">{exp.company}</div>
+                            <div className="info-page-text flex flex-col items-start justify-start">
+                                <div className="text-custom-light">{exp.role}</div>
+                                <div className="text-opacity-50 text-custom-light">{exp.startDate} - {exp.endDate}</div>
+                                <div className="text-opacity-50 text-custom-light">{exp.description}</div>
+                            </div>
+                        </div>
+                    );
+                })}
+            </div>
+        </>
+    );
 }
 
 export default InfoPage;

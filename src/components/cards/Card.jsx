2d65ac8be6704533f8507e561d3acb481e9f3231
Aidan Froggatt <aidanfr@live.ca>
2024-05-17 22:33:32 -0400

    added box shadow to tailwind config

--- src/components/cards/Card.jsx
+++ src/components/cards/Card.jsx
@@ -2,7 +2,7 @@ import '../../styles/components/Card.css'
 
 const ProjectCard = ({ className, children }) => {
     return (
-        <div className="card-container">
+        <div className="card-container shadow-1 ">
             <div className={`card ${className}`}>
                 {children}
             </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,9 @@ export default {
         'custom-dark': '#101010',
         'custom-light': '#f2f2f2',
       },
+      boxShadow: {
+        '1': "inset 0 0 8px rgba(0, 0, 0, .4), 0 0 60px rgba(0, 0, 0, .2), 0 30px 120px rgba(0, 0, 0, .8)"
+      }
     },
   },
   plugins: [],

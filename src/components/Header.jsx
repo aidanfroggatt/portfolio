3ab551efc3a5654d765b2e835f3699e32a30af3e
Aidan Froggatt <aidanfr@live.ca>
2024-05-26 17:36:55 -0400

    created morphing icon component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,6 +5,7 @@ import { GeneralInfoContext } from "../contexts/GeneralInfoContext.jsx";
 import { FaArrowUpLong } from "react-icons/fa6";
 import { motion } from "framer-motion";
 import {FaAt, FaTimes} from "react-icons/fa";
+import MorphingIcon from "./MorphingIcon.jsx";
 
 const Header = () => {
     const location = useLocation(); // Get current location
@@ -86,28 +87,9 @@ const Header = () => {
             {/*<div className="md:hidden fixed sm:right-12 right-6 flex justify-center items-center font-medium bg-custom-light w-12 h-12 rounded-full bg-opacity-5 text-sm border border-opacity-10 border-custom-light backdrop-blur">*/}
             {/*    <FaAt className={"w-9 h-9"}/>*/}
             {/*</div>*/}
-            <MorphingIcon/>
+            <MorphingIcon initialIcon={<FaTimes className="w-9 h-9"/>} toggleIcon={<FaAt className="w-9 h-9"/>}/>
         </div>
     )
 }
-
-const MorphingIcon = () => {
-    const [isToggled, setIsToggled] = useState(false);
-
-    const handleClick = () => {
-        setIsToggled(!isToggled);
-    };
-
-    return (
-        <div className="md:hidden fixed sm:right-12 right-6 flex justify-center items-center font-medium bg-custom-light w-12 h-12 rounded-full bg-opacity-5 text-sm border border-opacity-10 border-custom-light backdrop-blur" onClick={handleClick}>
-            <motion.div
-                animate={{ rotate: isToggled ? 360 : 0 }}
-                transition={{ duration: 0.5 }}
-            >
-                {isToggled ? <FaTimes className="w-9 h-9" /> : <FaAt className="w-9 h-9" />}
-            </motion.div>
-        </div>
-    );
-};
 
 export default Header;

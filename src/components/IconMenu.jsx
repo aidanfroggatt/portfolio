2670d72a9144dd6174cbacc3cf40dec76171b353
Aidan Froggatt <aidanfr@live.ca>
2024-05-26 19:23:16 -0400

    fixed up icon menu

--- src/components/IconMenu.jsx
+++ src/components/IconMenu.jsx
@@ -5,48 +5,63 @@ import {TfiArrowTopRight} from "react-icons/tfi";
 const IconMenu = ({ initialIcon, toggleIcon, menuOptions }) => {
     const [isToggled, setIsToggled] = useState(false);
 
+    return (
+        <>
+            {IconMorph(initialIcon, toggleIcon, [isToggled, setIsToggled])}
+            {isToggled && MenuModal(menuOptions)}
+        </>
+    );
+};
+
+const IconMorph = (initialIcon, toggleIcon, [isToggled, setIsToggled]) => {
     const handleClick = () => {
         setIsToggled(!isToggled);
     };
 
     return (
-        <>
-            <div
-                className={`flex justify-center items-center font-medium bg-custom-light w-12 h-12 rounded-full bg-opacity-5 text-sm border border-opacity-10 border-custom-light backdrop-blur`}
-                onClick={handleClick}
+        <div
+            className={`flex justify-center items-center font-medium bg-custom-light w-12 h-12 rounded-full bg-opacity-5 text-sm border border-opacity-10 border-custom-light backdrop-blur`}
+            onClick={handleClick}
+            key={"icon-morph"}
+        >
+            <motion.div
+                animate={{rotate: isToggled ? 360 : 0}}
+                transition={{duration: 0.5}}
             >
-                <motion.div
-                    animate={{ rotate: isToggled ? 360 : 0 }}
-                    transition={{ duration: 0.5 }}
-                >
-                    {isToggled ? initialIcon : toggleIcon}
-                </motion.div>
-            </div>
-            {isToggled && (
-                <motion.div
-                    className="absolute flex top-full w-32 mt-2 p-4 gap-y-4 right-0 flex-col justify-center items-start font-medium bg-custom-light rounded-xl bg-opacity-5 text-sm border border-opacity-10 border-custom-light backdrop-blur"
-                    initial={{
-                        opacity: 0,
-                        scale: 0,
-                        originX: 1,
-                        originY: 0,
-                    }}
-                    animate={{
-                        opacity: 1,
-                        scale: 1,
-                        transition: {
-                            duration: 0.5, // Animation duration for the "in" transition
-                        },
-                    }}
-                    exit={{
-                        opacity: 0,
-                        scale: 0,
-                        transition: {
-                            duration: 0.25, // Animation duration for the "out" transition
-                        },
-                    }}
-                >
-                    {Object.entries(menuOptions).map(([name, link], index) => (
+                {isToggled ? initialIcon : toggleIcon}
+            </motion.div>
+        </div>
+    );
+};
+
+const MenuModal = (menuOptions) => {
+    return (
+        <motion.div
+            key={"menu-modal"}
+            className="absolute flex top-full w-32 mt-2 p-4 gap-y-4 right-0 flex-col justify-center items-start font-medium bg-custom-light rounded-xl bg-opacity-5 text-sm border border-opacity-10 border-custom-light backdrop-blur"
+            initial={{
+                opacity: 0,
+                scale: 0,
+                originX: 1,
+                originY: 0,
+            }}
+            animate={{
+                opacity: 1,
+                scale: 1,
+                transition: {
+                    duration: 0.5, // Animation duration for the "in" transition
+                },
+            }}
+            exit={{
+                opacity: 0,
+                scale: 0,
+                transition: {
+                    duration: 0.25, // Animation duration for the "out" transition
+                },
+            }}
+        >
+            {Object.entries(menuOptions).map(([name, link], index) => {
+                    return (
                         <div className="flex flex-row w-full justify-between items-center">
                             <a
                                 key={index}
@@ -59,10 +74,10 @@ const IconMenu = ({ initialIcon, toggleIcon, menuOptions }) => {
                             </a>
                             <TfiArrowTopRight style={{ height: "0.875rem", width: "auto" }} />
                         </div>
-                    ))}
-                </motion.div>
+                    )
+                }
             )}
-        </>
+        </motion.div>
     );
 };
 

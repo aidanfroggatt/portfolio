f83826baab951ba740766c3a6785ce18d9961fd0
Aidan Froggatt <aidanfr@live.ca>
2024-05-30 21:52:01 -0400

    fixed card hover colouring

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,9 @@
 import '../styles/components/Card.css'
 
-const Card = ({ cardContainerClassName, cardContentClassName, children, onClick }) => {
+const Card = ({ cardContainerClassName, cardContentClassName, children, onClick, cardContainerStyle, cardContentStyle }) => {
     return (
-        <div onClick={onClick} className={`card-container shadow-card-mobile md:shadow-card ${cardContainerClassName}`}>
-            <div className={`card ${cardContentClassName}`}>
+        <div onClick={onClick} className={`card-container shadow-card-mobile md:shadow-card ${cardContainerClassName}`} style={cardContainerStyle}>
+            <div className={`card ${cardContentClassName}`} style={cardContentStyle}>
                 {children}
             </div>
         </div>

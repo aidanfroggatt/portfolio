e257d4779b8ce6549bc5bfc746fa7dfc698d36ca
Aidan Froggatt <aidanfr@live.ca>
2026-01-19 17:34:26 -0500

    updating tootlips to work on mobile

--- app/components/ui/tooltip.tsx
+++ app/components/ui/tooltip.tsx
@@ -9,7 +9,7 @@ const Tooltip = ({ children, text }: TooltipProps) => {
   return (
     <div className="relative group">
       {children}
-      <div className="absolute left-1/2 mt-1 -translate-x-1/2 top-full w-max px-2 py-1 text-white text-sm rounded bg-custom-light/5 border border-custom-light/10 backdrop-blur opacity-0 group-hover:opacity-100 transition-opacity duration-200 pointer-events-none">
+      <div className="absolute left-1/2 mt-1 -translate-x-1/2 top-full w-max px-2 py-1 text-white text-sm rounded bg-custom-light/5 border border-custom-light/10 backdrop-blur opacity-0 group-active:opacity-100 group-focus:opacity-100 group-hover:opacity-100 transition-opacity duration-200 pointer-events-none">
         {text}
       </div>
     </div>
